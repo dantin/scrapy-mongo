55c63ec717a344d581aed7a69ad9e0fd7d8b5f67
david <chengjie.ding@gmail.com>
2016-01-21 15:11:42 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
+version = '0.0.1'
+
 setup(
         name='scrapy-utils',
-        version="0.0.1",
+        author='david_ding',
+        author_email='chengjie.ding@gmail.com',
+        version=version,
         packages=find_packages('src'),
         package_dir={'': 'src'},
         package_data={
@@ -11,5 +15,6 @@ setup(
             '': ['*.txt'],
             # 包含scrapy_random_useragent包,data文件夹中的 *.dat文件
             'scrapy_random_useragent': ['data/*.dat'],
-        }
+        },
+        install_requires=['pymongo>=3.2', 'Scrapy>=1.0.4'],
 )
